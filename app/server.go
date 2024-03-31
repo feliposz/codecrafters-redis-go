253772df8276eda66973bf08074f975134b6e3c7
feliposz <feliposz@gmail.com>
2024-03-31 17:06:46 -0300

    Read a key

--- app/server.go
+++ app/server.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bufio"
 	"encoding/hex"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"math/rand"
 	"net"
 	"os"
+	"path/filepath"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -54,6 +58,10 @@ func main() {
 		}
 	}
 
+	store = make(map[string]string)
+	ttl = make(map[string]time.Time)
+	ackReceived = make(chan bool)
+
 	if config.role == "slave" {
 		masterConn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", config.replicaofHost, config.replicaofPort))
 		if err != nil {
@@ -93,16 +101,20 @@ func main() {
 		go handlePropagation(reader, masterConn)
 	}
 
+	if len(config.dir) > 0 && len(config.dbfilename) > 0 {
+		rdbPath := filepath.Join(config.dir, config.dbfilename)
+		err := readRDB(rdbPath)
+		if err != nil {
+			fmt.Printf("Failed to load '%s': %v\n", rdbPath, err)
+		}
+	}
+
 	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", config.port))
 	if err != nil {
 		fmt.Printf("Failed to bind to port %d\n", config.port)
 		os.Exit(1)
 	}
 	fmt.Println("Listening on: ", listener.Addr().String())
-
-	store = make(map[string]string)
-	ttl = make(map[string]time.Time)
-	ackReceived = make(chan bool)
 
 	for id := 1; ; id++ {
 		conn, err := listener.Accept()
@@ -278,6 +290,12 @@ func handleCommand(cmd []string) (response string, resynch bool) {
 		case "dbfilename":
 			response = encodeStringArray([]string{"dbfilename", config.dbfilename})
 		}
+	case "KEYS":
+		keys := make([]string, 0, len(store))
+		for key := range store {
+			keys = append(keys, key)
+		}
+		response = encodeStringArray(keys)
 	}
 	if isWrite {
 		propagate(cmd)
@@ -394,4 +412,161 @@ func handlePropagation(reader *bufio.Reader, masterConn net.Conn) {
 		}
 		replicaOffset += cmdSize
 	}
+}
+
+func readEncodedInt(reader *bufio.Reader) (int, error) {
+	mask := byte(0b11000000)
+	b0, err := reader.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	if b0&mask == 0b00000000 {
+		return int(b0), nil
+	} else if b0&mask == 0b01000000 {
+		b1, err := reader.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+		return int(b1)<<6 | int(b0&mask), nil
+	} else if b0&mask == 0b10000000 {
+		b1, _ := reader.ReadByte()
+		b2, _ := reader.ReadByte()
+		b3, _ := reader.ReadByte()
+		b4, err := reader.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+		// TODO: check endianness!
+		return int(b1)<<24 | int(b2)<<16 | int(b3)<<8 | int(b4), nil
+	} else if b0 >= 0b11000000 && b0 <= 0b11000010 { // Special format: Integers as String
+		var b1, b2, b3, b4 byte
+		b1, err = reader.ReadByte()
+		if b0 >= 0b11000001 {
+			b2, err = reader.ReadByte()
+		}
+		if b0 == 0b11000010 {
+			b3, _ = reader.ReadByte()
+			b4, err = reader.ReadByte()
+		}
+		if err != nil {
+			return 0, err
+		}
+		return int(b1) | int(b2)<<8 | int(b3)<<16 | int(b4)<<24, nil
+	} else {
+		return 0, errors.New("not implemented")
+	}
+}
+
+func readEncodedString(reader *bufio.Reader) (string, error) {
+	size, err := readEncodedInt(reader)
+	if err != nil {
+		return "", err
+	}
+	data := make([]byte, size)
+	actual, err := reader.Read(data)
+	if err != nil {
+		return "", err
+	}
+	if int(size) != actual {
+		return "", errors.New("unexpected string length")
+	}
+	return string(data), nil
+}
+
+func readRDB(rdbPath string) error {
+	file, err := os.Open(rdbPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	header := make([]byte, 9)
+	reader.Read(header)
+	if slices.Compare(header[:5], []byte("REDIS")) != 0 {
+		return errors.New("not a RDB file")
+	}
+
+	version, _ := strconv.Atoi(string(header[5:]))
+	fmt.Printf("File version: %d\n", version)
+
+	for eof := false; !eof; {
+
+		startDataRead := false
+		opCode, err := reader.ReadByte()
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		// TODO: handle errors properly
+		switch opCode {
+		case 0xFA: // Auxiliary fields
+			key, _ := readEncodedString(reader)
+			switch key {
+			case "redis-ver":
+				value, _ := readEncodedString(reader)
+				fmt.Printf("Aux: %s = %v\n", key, value)
+			case "redis-bits":
+				bits, _ := readEncodedInt(reader)
+				fmt.Printf("Aux: %s = %v\n", key, bits)
+			case "ctime":
+				ctime, _ := readEncodedInt(reader)
+				fmt.Printf("Aux: %s = %v\n", key, ctime)
+			case "used-mem":
+				usedmem, _ := readEncodedInt(reader)
+				fmt.Printf("Aux: %s = %v\n", key, usedmem)
+			case "aof-preamble":
+				size, _ := readEncodedInt(reader)
+				// preamble := make([]byte, size)
+				// reader.Read(preamble)
+				fmt.Printf("Aux: %s = %d\n", key, size)
+			default:
+				fmt.Printf("Unknown auxiliary field: %q\n", key)
+			}
+
+		case 0xFB: // Hash table sizes for the main keyspace and expires
+			keyspace, _ := readEncodedInt(reader)
+			expires, _ := readEncodedInt(reader)
+			fmt.Printf("Hash table sizes: keyspace = %d, expires = %d\n", keyspace, expires)
+			startDataRead = true
+
+		case 0xFE: // Database Selector
+			db, _ := readEncodedInt(reader)
+			fmt.Printf("Database Selector = %d\n", db)
+
+		case 0xFF: // End of the RDB file
+			eof = true
+		default:
+			fmt.Printf("Unknown op code: %d\n", opCode)
+		}
+
+		if startDataRead {
+			for {
+				valueType, err := reader.ReadByte()
+				if err != nil {
+					return err
+				}
+
+				// TODO: handle expiry
+
+				if valueType > 14 {
+					startDataRead = false
+					reader.UnreadByte()
+					break
+				}
+
+				key, _ := readEncodedString(reader)
+				value, _ := readEncodedString(reader)
+				fmt.Printf("Reading key/value: %q => %q\n", key, value)
+				store[key] = value
+			}
+		}
+	}
+
+	return nil
 }
