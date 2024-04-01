35c5c9ed7f306f480f9422e2dd6e380e52bfddab
feliposz <feliposz@gmail.com>
2024-03-31 23:34:44 -0300

    Partially auto-generated IDs

--- app/server.go
+++ app/server.go
@@ -380,29 +380,47 @@ func newStream() *stream {
 	}
 }
 
-func (s *stream) getNext(id string) (millisecondsTime, sequenceNumber uint64) {
+func (s *stream) getNext(id string) (millisecondsTime, sequenceNumber uint64, err error) {
 	parts := strings.Split(id, "-")
-	millisecondsTime, _ = strconv.ParseUint(parts[0], 10, 64)
-	sequenceNumber, _ = strconv.ParseUint(parts[1], 10, 64)
+
+	if len(parts) == 1 && parts[0] == "*" {
+		millisecondsTime = uint64(time.Now().UnixMilli())
+	} else if len(parts) == 2 && parts[1] == "*" {
+		millisecondsTime, _ = strconv.ParseUint(parts[0], 10, 64)
+		if millisecondsTime == s.last[0] {
+			sequenceNumber = s.last[1] + 1
+		} else if millisecondsTime > s.last[0] {
+			sequenceNumber = 0
+		} else {
+			return 0, 0, fmt.Errorf("The ID specified in XADD is equal or smaller than the target stream top item")
+		}
+	} else {
+		millisecondsTime, _ = strconv.ParseUint(parts[0], 10, 64)
+		sequenceNumber, _ = strconv.ParseUint(parts[1], 10, 64)
+	}
+
+	if millisecondsTime == 0 && sequenceNumber == 0 {
+		return 0, 0, fmt.Errorf("The ID specified in XADD must be greater than 0-0")
+	}
+
+	if millisecondsTime < s.last[0] || millisecondsTime == s.last[0] && sequenceNumber <= s.last[1] {
+		return 0, 0, fmt.Errorf("The ID specified in XADD is equal or smaller than the target stream top item")
+	}
+
 	return
 }
 
 func (s *stream) addStreamEntry(id string) (*streamEntry, error) {
-	millisecondsTime, sequenceNumber := s.getNext(id)
-	if millisecondsTime == 0 && sequenceNumber == 0 {
-		return nil, fmt.Errorf("The ID specified in XADD must be greater than 0-0")
+	millisecondsTime, sequenceNumber, err := s.getNext(id)
+	if err != nil {
+		return nil, err
 	}
 
 	if s.first[0] == 0 && s.first[1] == 0 {
 		s.first[0], s.first[1] = millisecondsTime, sequenceNumber
-		s.last[0], s.last[1] = millisecondsTime, sequenceNumber
-	} else if millisecondsTime > s.last[0] {
-		s.last[0], s.last[1] = millisecondsTime, sequenceNumber
-	} else if millisecondsTime == s.last[0] && sequenceNumber > s.last[1] {
-		s.last[0], s.last[1] = millisecondsTime, sequenceNumber
-	} else {
-		return nil, fmt.Errorf("The ID specified in XADD is equal or smaller than the target stream top item")
 	}
+	s.last[0], s.last[1] = millisecondsTime, sequenceNumber
+
 	entry := new(streamEntry)
 	entry.id[0] = millisecondsTime
 	entry.id[1] = sequenceNumber
