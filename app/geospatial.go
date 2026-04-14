52e2b31b2ca6acd92682181032851bc70fbda3f5
feliposz <feliposz@gmail.com>
2026-04-14 17:37:57 -0300

    Calculate location score

--- app/geospatial.go
+++ app/geospatial.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -9,6 +10,8 @@ const longitudeMax = 180
 const longitudeMin = -180
 const latitudeMax = 85.05112878
 const latitudeMin = -85.05112878
+const longitudeRange = longitudeMax - longitudeMin
+const latitudeRange = latitudeMax - latitudeMin
 
 func (srv *serverState) GeoAdd(key string, longStr string, latStr string, member string) (response string) {
 	longitude, err := strconv.ParseFloat(longStr, 64)
@@ -30,9 +33,29 @@ func (srv *serverState) GeoAdd(key string, longStr string, latStr string, member
 		response = encodeError(fmt.Errorf("invalid latitude value"))
 		return
 	}
-	var score float64
+	score := GeoHash(longitude, latitude)
 	set := srv.getSortedSet(key, true)
 	count := set.Put(score, member)
 	response = encodeInt(count)
+	return
+}
+
+const hashBits = 26
+
+func GeoHash(longitude, latitude float64) float64 {
+	normRange := math.Pow(2, hashBits)
+	normLong := normRange * (longitude - longitudeMin) / longitudeRange
+	normLat := normRange * (latitude - latitudeMin) / latitudeRange
+	truncLong := uint32(normLong)
+	truncLat := uint32(normLat)
+	interleaved := interleaveBits(truncLat, truncLong)
+	return float64(interleaved)
+}
+
+func interleaveBits(x, y uint32) (result uint64) {
+	for i := 0; i < hashBits+1; i++ {
+		result |= uint64(x>>i&1) << (i * 2)
+		result |= uint64(y>>i&1) << (i*2 + 1)
+	}
 	return
 }
